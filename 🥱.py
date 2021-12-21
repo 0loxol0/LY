f023bc3d7338a4c4afa17c8b08a38a08d4f7650c
Ahmedalzwage <72076217+Ahmedalzwage@users.noreply.github.com>
2021-12-21 16:25:31 +0200

    Add files via upload

--- 🥱.py
+++ 🥱.py
@@ -431,7 +431,7 @@ def cek_opsi(user,pw):
 	global loop,ubahP,pwbaru
 	session=requests.Session()
 	session.headers.update({
-		"Host":"m.facebook.com",
+		"Host":"https://b-api.facebook.com",
 		"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 		"accept-encoding":"gzip, deflate",
 		"accept-language":"en-US;q=0.9",
